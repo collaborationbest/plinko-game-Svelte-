400933f12268e90791dc59da5d5bc82dbaf37d5a
jericoAragonSenior <jericoaragon.senior@gmail.com>
2025-02-04 14:03:20 -0800

    Update eth to sol

--- src/lib/components/Balance.svelte
+++ src/lib/components/Balance.svelte
@@ -1,69 +1,36 @@
 <script lang="ts">
-  // import { Connection, PublicKey } from '@solana/web3.js';
-	// import { walletStore } from '@svelte-on-solana/wallet-adapter-core';
-	// import {
-	// 	workSpace,
-	// 	WalletProvider,
-	// 	WalletMultiButton,
-	// 	ConnectionProvider
-	// } from '@svelte-on-solana/wallet-adapter-ui';
-	// import { clusterApiUrl } from '@solana/web3.js';
-	// import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
-  // import {
-  //   balance,
-  // } from '$lib/stores/game';
+  import { createAppKit } from '@reown/appkit'
+import { SolanaAdapter } from '@reown/appkit-adapter-solana'
+import { solana, solanaTestnet, solanaDevnet } from '@reown/appkit/networks'
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
 
-	// const localStorageKey = 'walletAdapter';
-	// const network = clusterApiUrl('devnet'); // localhost or mainnet
-
-	// let wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
-import { createAppKit } from '@reown/appkit'
-import { mainnet, arbitrum } from '@reown/appkit/networks'
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-
-// 1. Get a project ID at https://cloud.reown.com
-const projectId = '9a215fccbadae1b71fd30d85e929458b'
-
-export const networks = [mainnet, arbitrum]
-
-// 2. Set up Wagmi adapter
-const wagmiAdapter = new WagmiAdapter({
-  projectId,
-  networks
+// 0. Set up Solana Adapter
+const solanaWeb3JsAdapter = new SolanaAdapter({
+  wallets: [new PhantomWalletAdapter(), new SolflareWalletAdapter()]
 })
 
-// 3. Configure the metadata
+// 1. Get projectId from https://cloud.reown.com
+const projectId = 'ec3f23d26684047304a84d4817a9d9ba'
+
+// 2. Create a metadata object - optional
 const metadata = {
   name: 'AppKit',
-  description: 'AppKit Example',
+  description: 'AppKit Solana Example',
   url: 'https://example.com', // origin must match your domain & subdomain
   icons: ['https://avatars.githubusercontent.com/u/179229932']
 }
 
-// 3. Create the modal
-const modal = createAppKit({
-  adapters: [wagmiAdapter],
-  networks: [mainnet, arbitrum],
-  metadata,
+// 3. Create modal
+createAppKit({
+  adapters: [solanaWeb3JsAdapter],
+  networks: [solana, solanaTestnet, solanaDevnet],
+  metadata: metadata,
   projectId,
   features: {
     analytics: true // Optional - defaults to your Cloud configuration
   }
 })
-  // $: {
-  //   if ($walletStore?.connected) {
-  //     const publicKey = $walletStore.publicKey;
-  //     console.log(publicKey)
-  //     const connection = new Connection(network, 'confirmed');
 
-  //     connection.getBalance(publicKey).then((bal) => {
-  //       $balance = bal / 1e9; // Convert lamports to SOL
-  //       console.log(`Current balance: ${$balance} SOL`);
-  //     }).catch((err) => {
-  //       console.error('Error fetching balance:', err);
-  //     });
-  //   }
-  // }
 
 </script>
 
